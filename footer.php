9fee206e9b34d394c4feec630401bca527ae97a6
unknown <timwuchia@gmail.com>
2022-12-08 09:07:24 -0500

    change footer logo

--- footer.php
+++ footer.php
@@ -12,10 +12,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 ?>
 <footer>
-    <div class="footer-logo"><h2>English Blog</h2></div>
+    <div class="footer-logo">
+        <img src="<?php echo get_stylesheet_directory_uri() . '/assets/images/logo100x100.png'?>" alt="">
+    </div>
     <div class="site-legal">
-        <a href="/terms">條款</a> | <a href="/privacy-policy">隱私</a>
-        <p class="no-margin">&copy; All rights reserverd</p>
+        <a href="/privacy-policy">隱私政策</a>
+        <p class="no-margin">&copy; All rights reserved. 版權所有，禁止擅自轉貼節錄</p>
     </div>
 </footer>
 <?php wp_footer(); ?>
